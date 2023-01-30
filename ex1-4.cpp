8b94a28ce45a088487c8f91ad71188e30db279b4
shanxian333 <123912264+shanxian333@users.noreply.github.com>
2023-01-30 13:33:13 +0800

    Create ex1-4.cpp

---
+++ ex1-4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#include <stdio.h>
+int main(){
+    int a, b;
+    printf("A=> ");
+    scanf("%d", &a);
+    printf("B=> ");
+    scanf("%d", &b);
+    printf("A+B=%d\n", a+b);
+    return 0;
+}
