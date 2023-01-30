8e0e74ff99bdef856ec9104ab61aa7d1725202b9
shanxian333 <123912264+shanxian333@users.noreply.github.com>
2023-01-30 11:39:44 +0800

    Create ex1-3.cpp

---
+++ ex1-3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World shanxian, your height is" << 1 << "m" << 60 << "cm" << endl;
+    printf("Hello World shanxian, your height is %d m %d cm",1,60);
+    printf("Hello World shanxian, your height is %d m %d cm \n",1,60);
+    
+    printf("my height is %d m %d cm \n",1,60);
+    printf("the building is %10d cm \n",890);
+    printf("the building is %10d cm \n",7890);
+    printf("the building is %10d cm \n",67890);
+    printf("the building is %10d cm \n",567890);
+    printf("the building is %10d cm \n",1234567890);
+    
+    printf("pi=%.9f\n",3.1415926535897932384626433832795);
+    return 0;
+}
